efd35d48826ddb8c41131e12c3fed88096096708
Seb Carss <seb.carss@gmail.com>
2025-09-22 21:48:25 +0100

    Refactor to tidy up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 const GITLAB_API_TOKEN = process.env.GITLAB_API_TOKEN;
+const GITLAB_URL = 'https://gitlab.com/api/v4';
 
 if (!GITLAB_API_TOKEN) {
     throw new Error("GITLAB_API_TOKEN is not set");
@@ -20,8 +23,8 @@ async function callApi(endpoint: string, accessToken: string): Promise<any> {
     console.log('request made to web API at: ' + new Date().toString());
 
     try {
-        const response = await axios.get(endpoint, options);
-        return response.data;
+        const response = await axios.get(endpoint, { ...options, validateStatus: () => true });
+        return { data: response.data, headers: response.headers, status: response.status };
     } catch (error: any) {
         console.log(error);
         return error;
@@ -29,13 +32,23 @@ async function callApi(endpoint: string, accessToken: string): Promise<any> {
 }
 
 function main() {
-    console.log("GitLab API Token is set");
+    const outputDir = path.join('./', 'output');
+    const outputPath = path.join(outputDir, 'response.json');
 
-    // Example usage of callApi function
-    const exampleEndpoint = 'https://gitlab.com/api/v4/groups/115863492'; // Replace with a valid GitLab API endpoint
-    callApi(exampleEndpoint, GITLAB_API_TOKEN as string)
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+    }
+
+    const PER_PAGE = 1;
+    let pageNo = 1;
+
+
+    const gitlabEndpoint = `${GITLAB_URL}/groups/115863492/projects?include_subgroups=true&per_page=${PER_PAGE}&page=${pageNo}`;
+    callApi(gitlabEndpoint, GITLAB_API_TOKEN as string)
         .then(data => {
-            console.log("API Response:", data);
+            fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
+            console.log(`API response written to ${outputPath}`);
+            console.log(data.headers)
         })
         .catch(error => {
             console.error("Error calling API:", error);
